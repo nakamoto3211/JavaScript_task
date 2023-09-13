bf58c5ed3656e15553e544b7168958be3a7a29f7
EC2 Default User <ec2-user@ip-172-31-18-23.ap-northeast-1.compute.internal>
2023-09-13 03:24:34 +0000

    stopwatch_JS実践課題

---
+++ main.js
@@ -0,0 +1,63 @@
+/*global $*/
+$(document).ready(function(){
+  
+  var sec_OneTenth = 0;
+  var sec = 0;
+  var sec_TensPlace = 0;
+  var min = 0;
+  
+  var time_count;
+  
+  function Count() {
+    sec_OneTenth += 1;
+    
+    if (sec_OneTenth > 9) {
+      sec_OneTenth = 0;
+      sec += 1;
+    }
+    
+    if (sec > 9) {
+      sec = 0;
+      sec_TensPlace += 1;
+    }
+    
+    if (sec_TensPlace > 5) {
+      sec_TensPlace = 0;
+      min += 1;
+    }
+    
+    $('#timer').html(min + ':' +  sec_TensPlace + ':' + sec + ':' + sec_OneTenth);
+  }
+  
+  $("#start").click(function() {
+    // sec_OneTenth = 0;
+    // sec = 0;
+    // sec_TensPlace = 0;
+    // min = 0;
+    
+    // $("#timer").html();
+    time_count = setInterval(Count,100);
+    
+    $(this).attr("disabled", true);
+    $("#stop, #reset").removeAttr("disabled");
+  });
+  
+  $("#stop").click(function() {
+    clearInterval(time_count);
+    
+    $(this).attr("disabled", true);
+    $("#start").attr("disabled",false);
+  });
+  
+  $("#reset").click(function() {
+    clearInterval(time_count);
+    sec_OneTenth = 0;
+    sec = 0;
+    sec_TensPlace = 0;
+    min = 0;
+    $("#timer").html("0:0:0:0");
+    
+    $("#reset,#stop").attr("disabled", true);
+    $("#start").attr("disabled",false);
+  });
+});
